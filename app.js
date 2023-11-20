1dc175f9ba08027ce3171a1c203b1648e3652ea7
rcastola <r.castola@gmail.com>
2023-11-20 14:49:56 +0000

    complete task 3

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const { getTopics } = require("./controllers/topics.controller");
 const { handleServerErrors } = require("./errors");
+const { getApis } = require("./controllers/apis.controllers");
 
 const app = express();
 
 app.use(express.json());
+app.get("/api", getApis);
 
 app.get("/api/topics", getTopics);
 
