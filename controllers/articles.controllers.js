1e145a6dde39ec4161112c882d094b5e5498f7a2
rcastola <r.castola@gmail.com>
2023-11-21 17:06:06 +0000

    update main after task 5 feedback

--- controllers/articles.controllers.js
+++ controllers/articles.controllers.js
@@ -1,8 +1,21 @@
-const { selectArticleByID } = require("../models/articles.models");
+const {
+  selectArticleByID,
+  selectArticles,
+} = require("../models/articles.models");
 
 exports.getArticleByID = (req, res, next) => {
   const { article_id } = req.params;
   selectArticleByID(article_id)
+    .then((articles) => {
+      res.status(200).send({ articles });
+    })
+    .catch((err) => {
+      next(err);
+    });
+};
+
+exports.getArticles = (req, res, next) => {
+  selectArticles()
     .then((articles) => {
       res.status(200).send({ articles });
     })
